bf218002ff9bf20146bb23e21c8af4c25fd629e9
Adolfo <adolfo_0302@hotmail.com>
2022-01-27 09:02:22 -0600

    1.1  Analizador de Excel
    Programa base para la lectura de Excel (nuevo metodo encontrado)

---
+++ 1.1-Analizador Excel/1.1-Analizador Excel/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace _1._1_Analizador_Excel
+{
+    partial class frmVista
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtgTabla = new System.Windows.Forms.DataGridView();
+            this.btnCargar = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnVelocidadPromedio = new System.Windows.Forms.Button();
+            this.btnVelocidadMaxima = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgTabla)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dtgTabla
+            // 
+            this.dtgTabla.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgTabla.Location = new System.Drawing.Point(44, 122);
+            this.dtgTabla.Name = "dtgTabla";
+            this.dtgTabla.Size = new System.Drawing.Size(885, 510);
+            this.dtgTabla.TabIndex = 0;
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.Location = new System.Drawing.Point(44, 55);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(75, 23);
+            this.btnCargar.TabIndex = 1;
+            this.btnCargar.Text = "Cargar";
+            this.btnCargar.UseVisualStyleBackColor = true;
+            this.btnCargar.Click += new System.EventHandler(this.btnCargar_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(943, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(25, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnVelocidadPromedio
+            // 
+            this.btnVelocidadPromedio.Enabled = false;
+            this.btnVelocidadPromedio.Location = new System.Drawing.Point(757, 55);
+            this.btnVelocidadPromedio.Name = "btnVelocidadPromedio";
+            this.btnVelocidadPromedio.Size = new System.Drawing.Size(75, 37);
+            this.btnVelocidadPromedio.TabIndex = 3;
+            this.btnVelocidadPromedio.Text = "Promediar Velocidad";
+            this.btnVelocidadPromedio.UseVisualStyleBackColor = true;
+            this.btnVelocidadPromedio.Click += new System.EventHandler(this.btnVelocidadPromedio_Click);
+            // 
+            // btnVelocidadMaxima
+            // 
+            this.btnVelocidadMaxima.Enabled = false;
+            this.btnVelocidadMaxima.Location = new System.Drawing.Point(854, 55);
+            this.btnVelocidadMaxima.Name = "btnVelocidadMaxima";
+            this.btnVelocidadMaxima.Size = new System.Drawing.Size(75, 37);
+            this.btnVelocidadMaxima.TabIndex = 4;
+            this.btnVelocidadMaxima.Text = "Velocidad Maxima";
+            this.btnVelocidadMaxima.UseVisualStyleBackColor = true;
+            this.btnVelocidadMaxima.Click += new System.EventHandler(this.btnVelocidadMaxima_Click);
+            // 
+            // frmVista
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(980, 684);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnVelocidadMaxima);
+            this.Controls.Add(this.btnVelocidadPromedio);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnCargar);
+            this.Controls.Add(this.dtgTabla);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmVista";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.ShowIcon = false;
+            this.Load += new System.EventHandler(this.frmVista_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgTabla)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dtgTabla;
+        private System.Windows.Forms.Button btnCargar;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnVelocidadPromedio;
+        private System.Windows.Forms.Button btnVelocidadMaxima;
+    }
+}
+
---
+++ 1.1-Analizador Excel/1.1-Analizador Excel/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace _1._1_Analizador_Excel 
+{
+    using Excel = Microsoft.Office.Interop.Excel;
+    public partial class frmVista : Form
+    {
+        private DataTable dtExcel;
+        public frmVista()
+        {
+            InitializeComponent();
+        }
+
+        private void frmVista_Load(object sender, EventArgs e)
+        {
+    
+        }
+
+    
+        public DataTable LeerExcel(string fileName, string fileExt)
+        {
+            string conn = string.Empty;
+            DataTable dtexcel = new DataTable();
+            if (fileExt.CompareTo(".xls") == 0)
+            {
+                conn = @"provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + fileName + ";Extended Properties='Excel 8.0;HRD=Yes;IMEX=1';"; //excel 2007 o anterior
+            }
+            else
+            { 
+                conn = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + fileName + ";Extended Properties='Excel 12.0;HDR=NO';"; //excel 2007 y posterior
+            }
+            using (OleDbConnection con = new OleDbConnection(conn))
+            {
+                try
+                {
+                    Microsoft.Office.Interop.Excel.Application xlApp = new Microsoft.Office.Interop.Excel.Application();
+                    Microsoft.Office.Interop.Excel.Workbook excelBook = xlApp.Workbooks.Open(fileName);
+
+                    String[] excelSheets = new String[excelBook.Worksheets.Count];
+                    int i = 0;
+                    foreach (Microsoft.Office.Interop.Excel.Worksheet wSheet in excelBook.Worksheets)
+                    {
+                        excelSheets[i] = wSheet.Name;
+                        i++;
+                    }
+                    OleDbDataAdapter oleAdpt = new OleDbDataAdapter("select * from ["+ excelSheets[2]+ "$] where F2 = '~'" , con); //Leer la hoja asignada
+                    oleAdpt.Fill(dtexcel); //rellenar el datatable
+                    btnVelocidadMaxima.Enabled = true;
+                    btnVelocidadPromedio.Enabled = true; 
+                }
+                catch(Exception e) {
+                    MessageBox.Show(e.Message);
+                    btnVelocidadMaxima.Enabled = false;
+                    btnVelocidadPromedio.Enabled = false;
+                }
+            }
+            dtExcel = dtexcel;
+            return dtexcel;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnCargar_Click(object sender, EventArgs e)
+        {
+            string filePath = string.Empty;
+            string fileExt = string.Empty;
+            OpenFileDialog file = new OpenFileDialog(); 
+            if (file.ShowDialog() == System.Windows.Forms.DialogResult.OK) 
+            {
+                filePath = file.FileName; 
+                fileExt = Path.GetExtension(filePath); 
+                if (fileExt.CompareTo(".xls") == 0 || fileExt.CompareTo(".xlsx") == 0)
+                {
+                    try
+                    {
+                        DataTable dtExcel = new DataTable();
+                        dtExcel = LeerExcel(filePath, fileExt);
+                       
+                        dtgTabla.Visible = true;
+                        dtgTabla.DataSource = dtExcel;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message.ToString());
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Favor de elegir un archivo .xls o .xlsx .", "Atención", MessageBoxButtons.OK, MessageBoxIcon.Error); 
+                }
+            }
+        }
+
+        private void btnVelocidadPromedio_Click(object sender, EventArgs e)
+        {
+            Double dblPromedio = 0;
+            int intContador = 0;
+            foreach (DataRow row in dtExcel.Rows)
+            {
+                if (row[11].ToString() != "0" )
+                {
+                    dblPromedio += Double.Parse(row[11].ToString());
+                    intContador++;
+                }
+
+            }
+            MessageBox.Show(String.Format("La velocidad promedio es de {0} km/h",(dblPromedio/intContador)));
+        }
+
+        private void btnVelocidadMaxima_Click(object sender, EventArgs e)
+        {
+            Double dblMayor = 0;
+            foreach (DataRow row in dtExcel.Rows)
+            {
+                if (dblMayor < Double.Parse(row[11].ToString()))
+                {
+                    dblMayor = Double.Parse(row[11].ToString());
+                }
+            }
+            MessageBox.Show(String.Format("La velocidad maxima alcanzada es de {0} km/h ",dblMayor));
+        }
+    }
+}
